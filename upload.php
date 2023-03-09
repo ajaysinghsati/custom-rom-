1817010c559e3f15468235fad2a61a71b1ce7d33
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-03-09 05:38:05 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,3 +12,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $file_size = $_FILES["firmware"]["size"];
   
   // Check file type
+  if ($file_type != "application/octet-stream" && $file_type != "application/vnd.android.package-archive") {
+    echo "Error: Only binary files are allowed.";
+    exit;
+  }
+  
+  // Check file size
+  if ($file_size > 50000000) {
+    echo "Error: File size exceeds 50 MB.";
+    exit;
+  }
+  
+  // Generate unique file name
+  $file_path = "firmware/" . uniqid() . "-" . $file_name;
+  
+  // Move file to firmware directory
+  if (!move_uploaded_file($file_tmp, $file_path)) {
+    echo "Error: Failed to upload file.";
+    exit;
+  }
+  
+  // Save firmware data to CSV file
+  $csv_path = "firmware.csv";
+  $data = array($version, $description, $file_path);
+  $fp = fopen($csv_path, "a");
+  fputcsv($fp, $data);
+  fclose($fp);
+  
+  // Redirect to firmware page
+  header("Location: firmware.php");
+  exit;
+}
+?>
+
